e1561537b8d8ff2d18efc66a1ff975994a5b7d6f
JoaoTaranto <tarantoj04@gmail.com>
2025-08-15 10:08:56 -0300

    feat: add UpdateUserUseCase

--- src/use-cases/update-user.js
+++ src/use-cases/update-user.js
@@ -1,8 +1,8 @@
 import bcrypt from "bcrypt";
 
-import { EmailAlreadyInUseError } from "../errors/user";
-import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email";
-import { PostgresUpdateUserRepository } from "../repositories/postgres/update-user";
+import { EmailAlreadyInUseError } from "../errors/user.js";
+import { PostgresGetUserByEmailRepository } from "../repositories/postgres/get-user-by-email.js";
+import { PostgresUpdateUserRepository } from "../repositories/postgres/update-user.js";
 
 export class UpdateUserUseCase {
     async execute(userId, updateUserParams) {
@@ -14,7 +14,7 @@ export class UpdateUserUseCase {
                 updateUserParams.email,
             );
 
-            if (userWithProvidedEmail) {
+            if (userWithProvidedEmail && userWithProvidedEmail.id !== userId) {
                 throw new EmailAlreadyInUseError(updateUserParams.email);
             }
         }
