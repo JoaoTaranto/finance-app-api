eca4c18a73d104af74ad3069a357ae0d35f69dc3
JoaoTaranto <tarantoj04@gmail.com>
2025-08-20 17:53:49 -0300

    feat: add PostgresGetTransactionByIdRepository

---
+++ src/repositories/postgres/transactions/get-transaction-by-id.js
@@ -0,0 +1,11 @@
+import { PostgresHelper } from "../../../database/postgres/helper.js";
+
+export class PostgresGetTransactionByIdRepository {
+    async execute(transactionId) {
+        const transaction = await PostgresHelper.query(
+            "SELECT * FROM transactions WHERE id = $1",
+            [transactionId],
+        );
+        return transaction[0];
+    }
+}
