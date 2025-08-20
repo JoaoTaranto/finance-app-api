53a753c416e0466acf65971f09178aea88081df6
JoaoTaranto <tarantoj04@gmail.com>
2025-08-20 15:13:00 -0300

    feat: add UpdateTransactionController

---
+++ src/controllers/transactions/update-transaction.js
@@ -0,0 +1,57 @@
+import checkIfIdIsValid, {
+    checkIfAmountIsValid,
+    invalidAmountResponse,
+    IsIdInvalidResponse,
+    badRequest,
+    serverError,
+    checkIfTypeIsValid,
+    invalidTypeResponse,
+    ok,
+} from "../helpers/index.js";
+
+export class UpdateTransactionController {
+    constructor(updateTransactionUseCase) {
+        this.updateTransactionUseCase = updateTransactionUseCase;
+    }
+    async execute(httpRequest) {
+        try {
+            const isIdValid = checkIfIdIsValid(
+                httpRequest.params.transactionId,
+            );
+            if (!isIdValid) return IsIdInvalidResponse();
+
+            const params = httpRequest.body;
+
+            const allowedFields = ["name", "date", "amount", "type"];
+
+            const someFieldIsNotValid = Object.keys(params).some(
+                (field) => !allowedFields.includes(field),
+            );
+            if (someFieldIsNotValid) {
+                return badRequest({
+                    message: "Some provided field is not allowed",
+                });
+            }
+
+            if (params.amount) {
+                const amountIsValid = checkIfAmountIsValid(params.amount);
+                if (!amountIsValid) return invalidAmountResponse();
+            }
+
+            if (params.type) {
+                const typeIsValid = checkIfTypeIsValid(params.type);
+                if (!typeIsValid) return invalidTypeResponse();
+            }
+
+            const updatedTransaction =
+                await this.updateTransactionUseCase.execute(
+                    httpRequest.params.transactionId,
+                    params,
+                );
+            return ok(updatedTransaction);
+        } catch (error) {
+            console.error(error);
+            return serverError();
+        }
+    }
+}
