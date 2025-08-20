f84f5b2fc85b742f2ad5a642edc284a35ca9643c
JoaoTaranto <tarantoj04@gmail.com>
2025-08-20 15:16:49 -0300

    feat: create UpdateTransactionController factory

--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -2,16 +2,19 @@ import {
     PostgresCreateTransactionRepository,
     PostgresGetTransactionsByUserIdRepository,
     PostgresGetUserByIdRepository,
+    PostgresUpdateTransactionRepository,
 } from "../../repositories/postgres/index.js";
 
 import {
     CreateTransactionUseCase,
     GetTransactionsByUserIdUseCase,
+    UpdateTransactionUseCase,
 } from "../../use-cases/index.js";
 
 import {
     CreateTransactionController,
     GetTransactionsByUserIdController,
+    UpdateTransactionController,
 } from "../../controllers/index.js";
 
 export const makeCreateTransactionController = () => {
@@ -46,4 +49,19 @@ export const makeGetTransactionsByUserIdController = () => {
         new GetTransactionsByUserIdController(getTransactionsByUserIdUseCase);
 
     return getTransactionsByUserIdController;
+};
+
+export const makeUpdateTransactionController = () => {
+    const postgresUpdateTransactionRepository =
+        new PostgresUpdateTransactionRepository();
+
+    const updateTransactionUseCase = new UpdateTransactionUseCase(
+        postgresUpdateTransactionRepository,
+    );
+
+    const updateTransactionController = new UpdateTransactionController(
+        updateTransactionUseCase,
+    );
+
+    return updateTransactionController;
 };
