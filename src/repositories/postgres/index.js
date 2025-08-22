aaeede8eac4fca2ee8b17fd9ed9f900cfbea54e8
JoaoTaranto <tarantoj04@gmail.com>
2025-08-22 08:16:09 -0300

    feat: add GetUserBalanceUseCase

--- src/repositories/postgres/index.js
+++ src/repositories/postgres/index.js
@@ -3,10 +3,10 @@ export * from "./user/update-user.js";
 export * from "./user/get-user-by-id.js";
 export * from "./user/get-user-by-email.js";
 export * from "./user/delete-user.js";
+export * from "./user/get-user-balance.js";
 
 export * from "./transactions/create-transaction.js";
 export * from "./transactions/get-transaction-by-user-id.js";
 export * from "./transactions/update-transaction.js";
 export * from "./transactions/get-transaction-by-id.js";
 export * from "./transactions/delete-transaction.js";
-export * from "./user/get-user-balance.js";
